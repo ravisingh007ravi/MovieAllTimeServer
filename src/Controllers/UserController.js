4b55e828cad47f9c91277ba7c2421cbcd5ef0f44
Ravi Singh <ravisingh007ravi@gmail.com>
2025-02-04 15:38:44 +0530

    bbbbb

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -1,3 +1,100 @@
+const userModel = require("../Models/userModel.js")
+const { ImgUrl } = require('../Cloudinary/UploadImages.js');
+const { verifyOtp } = require('../Mail/sendMail.js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+
 exports.createUser = async (req, res) => {
-  res.send('hello Backend..') 
+    try {
+        const data = req.body;
+        const img = req.file;
+
+        const { name, email, password } = data;
+
+        const randonOtp = Math.floor(1000 + Math.random() * 9000);
+
+        const checkEmail = await userModel.findOneAndUpdate({ email: email }, { $set: { otp: randonOtp } }, { new: true });
+
+        if (checkEmail) {
+            if ((checkEmail.isAccountActive) == false) return res.status(200).send({ status: false, msg: "Your Account is Blocked" });
+            if ((checkEmail.isVerify) == true) return res.status(200).send({ status: false, msg: "Your Account is Verify pls LogIn" });
+
+            verifyOtp(name, email, randonOtp);
+            return res.status(200).send({ status: true, msg: "otp send successfully", id: checkEmail._id });
+        }
+
+        if (img) {
+            const urlPath = img.path;
+            const urlResult = await ImgUrl(urlPath);
+            data.profileImg = urlResult.secure_url;
+        }
+
+        const bcryptPass = await bcrypt.hash(password, 10);
+        data.password = bcryptPass;
+
+        data.otp = randonOtp;
+        data.role = 'user';
+        verifyOtp(name, email, randonOtp);
+        const userDB = await userModel.create(data);
+
+        const DB = {
+            profileImg: userDB.profileImg,
+            title: userDB.title,
+            name: userDB.name,
+            email: userDB.email,
+        }
+
+        res.status(201).send({ status: true, msg: "User created successfully", data: DB, id: userDB._id });
+    } catch (error) {
+        res.status(500).send({ status: false, message: error.message });
+    }
 };
+
+
+exports.userOTPVerify = async (req, res) => {
+    try {
+        const userid = req.params.id;
+        const { otp } = req.body;
+
+        if (!otp) return res.status(200).send({ status: true, msg: "pls Provide OTP" });
+
+        const userDB = await userModel.findById({ _id: userid });
+
+        if (!userDB) return res.status(200).send({ status: true, msg: "User not found" });
+
+        if (!((userDB.otp) == otp)) return res.status(200).send({ status: true, msg: "Wrong otp" });
+
+        await userModel.findByIdAndUpdate({ _id: userid }, { $set: { isVerify: true } }, { new: true });
+        res.status(200).send({ status: true, msg: "User Verify successfully" });
+
+    }
+    catch (error) {
+        res.status(500).send({ status: false, message: error.message });
+    }
+};
+
+exports.userLogIn = async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+
+        const checkEmail = await userModel.findOne({ email: email });
+        if (!checkEmail) return res.status(200).send({ status: false, msg: "User not found" });
+
+        if ((checkEmail.isAccountActive) == false) return res.status(200).send({ status: false, msg: "Your Account is Blocked" });
+        if ((checkEmail.isVerify) == false) return res.status(200).send({ status: false, msg: "pls Verify OTP" });
+
+        const checkPassword = await bcrypt.compare(password, checkEmail.password);
+        if (!checkPassword) return res.status(200).send({ status: false, msg: "Wrong Password" });
+
+        const userToken = jwt.sign({ UserId: checkEmail._id }, process.env.UserTokenKey, { expiresIn: '12h' })
+
+        res.status(200).send({ status: true, msg: "successfully Created Token", UserToken: userToken, UserId: checkEmail._id });
+    }
+
+    catch (error) {
+        res.status(500).send({ status: false, message: error.message });
+    }
+}   
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const multer = require('multer');
 const router = express.Router();
-const {createUser} = require('../Controllers/UserController')
-router.get('/createUser',createUser)
-// const { createUser, userOTPVerify, userLogIn } = require('../controller/UserController.js');
-// const { validUserData, validUserLogInData } = require('../Middleware/UserValidation.js');
+const {createUser,userOTPVerify, userLogIn} = require('../Controllers/UserController')
 
-// const upload = multer({ storage: multer.diskStorage({}) });
+// const { createUser, userOTPVerify, userLogIn } = require('../controller/UserController.js');
+const { validUserData, validUserLogInData } = require('../Middleware/UserValidation.js');
+
+const upload = multer({ storage: multer.diskStorage({}) });
 
 // // User API's
-// router.post('/createUser', upload.single(),validUserData, createUser);
-// router.post('/userOTPVerify/:id', userOTPVerify);
-// router.post('/userLogIn', upload.single(),validUserLogInData, userLogIn);
+router.post('/createUser', upload.single(),validUserData, createUser);
+router.post('/userOTPVerify/:id', userOTPVerify);
+router.post('/userLogIn', upload.single(),validUserLogInData, userLogIn);
 
 
 router.all('/*', (req, res) => { return res.status(404).send({ status: false, msg: 'Invalid Url' }) })
