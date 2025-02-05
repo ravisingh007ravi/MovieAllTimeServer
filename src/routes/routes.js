92f8ef6bb7ca18f722adeab152d774699b3f4df0
Ravi Singh <ravisingh007ravi@gmail.com>
2025-02-05 10:04:04 +0530

    a

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const multer = require('multer');
 const router = express.Router();
 const { createUser, userOTPVerify, userLogIn } = require('../Controllers/UserController');
-const { validUserData, validUserLogInData } = require('../Middleware/UserValidation.js');
+// const { validUserData, validUserLogInData } = require('../Middleware/UserValidation.js');
 
 const upload = multer({ storage: multer.diskStorage({}) });
 
 // User API's
-router.post('/createUser', upload.single('profileImage'), validUserData, createUser);
+router.post('/createUser', upload.single('profileImage'), createUser);
 router.post('/userOTPVerify/:id', userOTPVerify);
-router.post('/userLogIn', upload.none(), validUserLogInData, userLogIn);
+router.post('/userLogIn', upload.none(), userLogIn);
 
 router.all('/*', (req, res) => res.status(404).send({ status: false, msg: 'Invalid URL' }));
 
